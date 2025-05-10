b3bbcc3b1dda1a21ecbc1fce7efca1fca46bbb9d
mrMadMaster <orlov1988brest@gmail.com>
2025-05-10 16:29:05 +0300

    Проектная работа №1

---
+++ src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String model;
+    int speed;
+    public Car(String model, int speed){
+        this.model = model;
+        this.speed = speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        Car[] cars = new Car[3];
+
+        for (int i = 1; i <= 3; i++) {
+
+            String model;
+            while (true) {
+                System.out.println("Выберите марку " + i + " автомобиля");
+                model = scanner.nextLine();
+                if (model.trim().isEmpty()) {
+                    System.out.println("Неверная марка автомобиля");
+                } else {
+                    break;
+                }
+            }
+
+            int speed;
+            while (true) {
+                try {
+                    System.out.println("Укажите максимальную скорость " + i + " автомобиля");
+                    speed = Integer.parseInt(scanner.nextLine());
+                    if (speed > 250 || 0 > speed) {
+                        throw new Exception();
+                    } else {
+                        break;
+                    }
+                } catch (Exception ex) {
+                    System.out.println("Неверная скорость");
+                }
+            }
+
+            Car car = new Car(model, speed);
+            cars[i-1] = car;
+            race.race(cars[i-1].model, cars[i-1].speed);
+        }
+        System.out.println("Самый быстрый автомобиль: " + race.vinner);
     }
 }
